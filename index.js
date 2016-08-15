a7497dd23f22ac0104a2f3c6e27da2a3ce816b16
yucong02 <yucong02@baidu.com>
2016-08-15 17:03:03 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+/**
+ * Created by Moyu on 16/8/15.
+ */
+
+var net = require('net')
+var argv = require('minimist')(process.argv.slice(2))
+
+
+const client = net.connect({
+    port: argv.p || 9988,
+    host: argv.h || 'localhost'
+}, () => {
+    console.log(`connected ${client.remoteAddress}:${client.remotePort}`)
+})
+client.setEncoding(null)
+
+client.pipe(process.stdout)
+
+client.on('close', () => {
+    console.log(`${client.remoteAddress}:${client.remotePort} connection closed`)
+})
+
